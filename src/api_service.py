7feca8549037705585dbe142c31a8cb3f4d647e3
TheJudge26 <danialawny26@gmail.com>
2026-05-20 02:51:40 +0300

    log

--- src/api_service.py
+++ src/api_service.py
@@ -9,25 +9,25 @@ def get_live_country_data(country_code="EGY"):
     """
     print(f"📡 Connecting to World Bank API for {country_code}...")
     
-    # World Bank API Indicators
+    
     indicators = {
         "Population": "SP.POP.TOTL",
         "GDP": "NY.GDP.MKTP.CD",
-        "CO2_Emissions": "EN.ATM.CO2E.KT" # CO2 emissions in kilotons
+        "CO2_Emissions": "EN.ATM.CO2E.KT" 
     }
     
     live_data = {'Year': datetime.now().year}
     
     for feature_name, indicator_code in indicators.items():
-        # Build the API URL (asking for just the 1 most recent valid data point)
+        
         url = f"http://api.worldbank.org/v2/country/{country_code}/indicator/{indicator_code}?format=json&mrnev=1"
         
         try:
             response = requests.get(url)
-            response.raise_for_status() # Check for connection errors
+            response.raise_for_status() 
             data = response.json()
             
-            # Extract the actual value from the JSON response
+            
             if len(data) > 1 and data[1]:
                 value = data[1][0]['value']
                 live_data[feature_name] = value
@@ -39,15 +39,15 @@ def get_live_country_data(country_code="EGY"):
             print(f"❌ API Error for {feature_name}: {e}")
             live_data[feature_name] = 0.0
 
-    # Convert to a DataFrame so it matches what our Neural Network expects
+    
     df_live = pd.DataFrame([live_data])
-    print("✅ Live Data Acquired successfully!\n")
+    print(" Live Data Acquired successfully!\n")
     
     return df_live
 
-# --- TEST THE API ---
+
 if __name__ == "__main__":
-    # 'EGY' is the official code for Egypt
+    
     egypt_current_data = get_live_country_data("EGY")
     print("Current Live Data for Egypt:")
-    display(egypt_current_data) if 'display' in globals() else print(egypt_current_data)
+    print(egypt_current_data)
